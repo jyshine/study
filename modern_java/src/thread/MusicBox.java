c05704fcbd2d96f30b93a3284e409ceb2baf9e9a
jyshine <jyshine3@gmail.com>
2021-05-09 17:25:11 +0900

    feat:synchronized

--- modern_java/src/thread/MusicBox.java
+++ modern_java/src/thread/MusicBox.java
@@ -1,8 +1,8 @@
 package thread;
 
 public class MusicBox {
-    public void playMusicA() {
 
+    public synchronized void playMusicA() {
         try {
             for (int i = 0; i < 10; i++) {
                 System.out.println(" 신나는 음악");
@@ -13,7 +13,7 @@ public class MusicBox {
         }
     }
 
-    public void playMusicB() {
+    public synchronized void playMusicB() {
         try {
             for (int i = 0; i < 10; i++) {
                 System.out.println(" 조용한 음악");
@@ -27,7 +27,10 @@ public class MusicBox {
     public void playMusicC() {
         try {
             for (int i = 0; i < 10; i++) {
-                System.out.println(" 락 음악");
+                //블록 부분만 synchronized로 처리할 수 있다.
+                synchronized (this){
+                    System.out.println(" 락 음악");
+                }
                 Thread.sleep((int)(Math.random()*1000));
             }
         }catch (Exception e){
