f0dbdc76140baf1e68aa45eb046eee40e7aec7f2
jyshine <jyshine3@gmail.com>
2021-05-11 17:29:44 +0900

    feat:프로그래머- 음양더하기

--- problem_solving/algorithm/programmers/src/coding/Test.java
+++
@@ -1,4 +0,0 @@
-package coding;
-
-public class Test {
-}
--- problem_solving/algorithm/src/solution/programmers/음양더하기.java
+++ problem_solving/algorithm/src/solution/programmers/음양더하기.java
@@ -1,4 +1,33 @@
 package solution.programmers;
 
 public class 음양더하기 {
+
+    public static void main(String[] args) {
+        int[] absolutes = {1,2,3};
+        boolean[] signs = {false,false,true};
+        solution(absolutes,signs);
+    }
+
+    private static int solution(int[] absolutes, boolean[] signs) {
+        /**
+         * 1.숫자 반복 연산
+         * 2.조건 true + , flase -
+         */
+        int sum = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                sum = sum + absolutes[i];
+            } else {
+                sum = sum - absolutes[i];
+            }
+        }
+        return sum;
+    }
+
+    public static int solution2(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
+        return answer;
+    }
 }
